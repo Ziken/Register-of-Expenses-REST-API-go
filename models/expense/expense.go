30e09defc7df9aed2c6d7425afec119e714a6ea9
Ziken <patrykkazmierczak97@gmail.com>
2018-03-03 20:17:44 +0100

    Add Expense model

---
+++ models/expense/expense.go
@@ -0,0 +1,13 @@
+package expense
+
+import (
+	. "github.com/ziken/Register-of-Expenses-REST-API-go/db"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Expense struct {
+	Id      bson.ObjectId  `bson:"_id" json:"_id"`
+	Title   string  `bson:"title,omitempty" json:"title" validate:"required,gt=2"`
+	Amount  float32 `bson:"amount,omitempty" json:"amount" validate:"required,gt=0.01"`
+	SpentAt int     `bson:"spentAt,omitempty" json:"spentAt" validate:"required"`
+}
