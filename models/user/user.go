c1b0101298bdc6eed5acbefb8a6015e5b6284a81
Ziken <patrykkazmierczak97@gmail.com>
2018-03-08 10:13:08 +0100

    Add authentication token

--- models/user/user.go
+++ models/user/user.go
@@ -3,6 +3,11 @@ package user
 import (
 	"gopkg.in/mgo.v2/bson"
 
+	. "github.com/ziken/Register-of-Expenses-REST-API-go/db"
+
+	"gopkg.in/go-playground/validator.v9"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
 type User struct {
@@ -16,3 +21,42 @@ type Token struct {
 	Token  string `bson:"token" json:"-"`
 }
 
+
+var validate * validator.Validate
+
+func (usr * User) Validate() error {
+	return validate.Struct(usr)
+}
+
+func (usr *  User) GenerateAuthToken() (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"_id": usr.Id,
+		"access": "auth",
+	})
+
+	tokenString, err := token.SignedString([]byte("good-secret"))
+	if err != nil {
+		return "", err
+	}
+
+
+	err = DB.C(USER_COLLECTION).UpdateId(usr.Id, bson.M{
+		"$push": bson.M{
+			"tokens": Token{Token:tokenString, Access:"auth"},
+		},
+	})
+
+	return tokenString, err
+}
+
+func Save(userDoc User) (User, error) {
+	userDoc.Id = bson.NewObjectId()
+
+	err := DB.C(USER_COLLECTION).Insert(userDoc)
+
+	return userDoc, err;
+}
+
+func init() {
+	validate = validator.New()
+}
